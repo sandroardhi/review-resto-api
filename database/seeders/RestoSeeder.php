10fd058e9033b2b3b0dc7234d6bbc3ab1e6fa2f0
sandroardhi <sandrosaputra557@gmail.com>
2023-02-13 09:36:59 +0700

    feat(seeder): add resto seeder

--- database/seeders/RestoSeeder.php
+++ database/seeders/RestoSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Resto;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,6 +15,8 @@ class RestoSeeder extends Seeder
      */
     public function run()
     {
-        //
+        if(config('app.debug', false)) {
+            Resto::factory()->count(100)->create();
+        }
     }
 }
